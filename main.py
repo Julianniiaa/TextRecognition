0522b10b1cfc98979f63521bc5d0aabc39a13b9d
Julia <ykulichyk@yandex.by>
2023-06-19 15:51:16 +0300

    init

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 import cv2
 
+
 # plate = pt.image_to_string(threshold, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8')
 # pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
